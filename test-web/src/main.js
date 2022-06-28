83b78e557cd34f86a46b65cbf566c410d7fad4f3
18nguyenl <18nguyenl@gmail.com>
2022-06-28 18:18:29 -0500

    Add debug option to MIDI message parse

--- test-web/src/main.js
+++ test-web/src/main.js
@@ -1,7 +1,7 @@
 import { PhysicalPiano } from "../../dist/module";
 import { store, countReducer, incrementCount } from "../../dist/module";
 
-// const piano = new PhysicalPiano();
-// piano.requestMIDIAccess().then((midi) => {
-//     piano.listenValue()
-// })
+const piano = new PhysicalPiano();
+piano.requestMIDIAccess().then((midi) => {
+    piano.listenValue()
+})
